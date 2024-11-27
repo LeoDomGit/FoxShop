23c56a8914f295d1987c171e5abac1233398495c
Pham Ho Cong Khuong <congkhuong.dev@gmail.com>
2024-11-28 01:31:54 +0700

    Upload filter

--- src/pages/ProductByCatePage.jsx
+++ src/pages/ProductByCatePage.jsx
@@ -1,5 +1,4 @@
-// ProductPageByCategory.jsx
-
+/*eslint-disable*/
 import React, { useState, useEffect } from 'react';
 import axios from '../api/axios';
 import { useParams } from 'react-router-dom';

38ad9d75693f9c3a8bd6f442ca64e733ae28b0a6
Leo Git <leodomsolar@gmail.com>
2024-11-28 09:57:37 +0700

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,6 +2,5 @@ import axios from 'axios';
 
 export default axios.create({
   baseURL: 'https://dashboard.foxshop.one/api',
-  // baseURL: 'http://localhost:8000/api',
   withCredentials: true,
 });

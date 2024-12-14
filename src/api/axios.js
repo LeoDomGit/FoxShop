bd985db6c35841a3462d595155fa20afad3f7ed6
Pham Ho Cong Khuong <congkhuong.dev@gmail.com>
2024-12-14 21:03:36 +0700

    Fix UI

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const baseURL = process.env.REACT_APP_API_URL;
 export default axios.create({
-  baseURL: baseURL,
-  // baseURL: 'http://localhost:8000/api',
+  // baseURL: baseURL,
+  baseURL: 'http://localhost:8000/api',
   withCredentials: true,
 });
--- src/pages/ProductByCatePage.jsx
+++ src/pages/ProductByCatePage.jsx
@@ -1,4 +1,3 @@
-
 /*eslint-disable*/
 import React, { useState, useEffect } from 'react';
 import axios from '../api/axios';
@@ -13,6 +12,7 @@ const ProductPageByCategory = () => {
   const [products, setProducts] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
+  const [categoryName, setCategoryName] = useState('');
 
   // Hàm gọi API để lấy sản phẩm theo danh mục
   const fetchProductsByCategory = async () => {
@@ -22,10 +22,20 @@ const ProductPageByCategory = () => {
           page: currentPage,
         },
       });
-      setProducts(response.data.data);
-      setTotalPages(response.data.meta.total_pages);
+
+      const productsData = response.data.products?.data || [];
+      const category = response.data.category || {
+        name: 'Danh mục không xác định',
+      };
+
+      setProducts(productsData);
+      setCategoryName(category.name);
+      setTotalPages(response.data.products?.last_page || 1);
     } catch (error) {
       console.error('Error fetching products:', error);
+      setProducts([]);
+      setCategoryName('Lỗi tải danh mục');
+      setTotalPages(1);
     }
   };
 
@@ -33,13 +43,17 @@ const ProductPageByCategory = () => {
     fetchProductsByCategory();
   }, [categorySlug, currentPage]);
 
+  useEffect(() => {
+    document.title = categoryName;
+  }, [categoryName]);
+
   return (
     <div className='flex flex-col min-h-screen'>
       <Header />
       <main className='flex-grow'>
-        <div className='container mx-auto lg:px-5 xl:px-24 md:px-4 px-5 mb-2 mt-[100px] xl:mb-5 lg:mb-5 md:mb-5'>
+        <div className='container mx-auto lg:px-5 xl:px-24 md:px-4 px-5 mb-2 mt-[140px] xl:mb-5 lg:mb-5 md:mb-5'>
           <h1 className='text-xl font-medium'>
-            Sản phẩm thuộc danh mục: {categorySlug}
+            Sản phẩm thuộc danh mục: {categoryName}
           </h1>
 
           <div className='grid grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-4 mt-4'>
